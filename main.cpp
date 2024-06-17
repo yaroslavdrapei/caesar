19b569a6935f561f2c1f892edc9596c653a36a85
yaroslavdrapei <yarikdrapey@gmail.com>
2024-06-17 21:35:43 +0300

    encrypt function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
-#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+
+char shift(char* chr, int key);
+
+char* encrypt(char* rawText, int key);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    char myText[] = "Roses are red, violets are blue";
+    char* result = encrypt(myText,1);
+    printf("%s", result);
+}
+
+char shift(char chr, int key) {
+    key = key % 26;
+    int shifted;
+
+    if ((int) chr > 96 && (int) chr < 123) {
+        shifted = ((int) chr + key);
+        if (shifted > 122) {
+            shifted = 96 + shifted % 122;
+        }
+        return (char) shifted;
+    }
+
+    if ((int) chr > 65 && (int) chr < 91) {
+        shifted = ((int) chr + key);
+        if (shifted > 90) {
+            shifted = 65 + shifted % 90;
+        }
+        return (char) shifted;
+    }
+
+    return chr;
+}
+
+char* encrypt(char* rawText, int key) {
+    char* result = (char*)calloc(strlen(rawText), sizeof(char));
+
+    for (int i = 0; i < strlen(rawText); i++) {
+        result[i] = shift(rawText[i], key);
+    }
+
+    return result;
 }
